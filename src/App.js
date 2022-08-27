f3335598ffd3b7d0cd33b6a52921adca36bca727
DevanshSK <devanshs843@gmail.com>
2022-08-27 18:31:09 +0530

    Music data added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+// Import styles
+import "./styles/app.scss";
 // Adding components
 import Player from "./components/Player";
 import Song from "./components/Song";
+// Import Data
+import data from "./util";
 
 function App() {
   return (
---
+++ src/util.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      //1
+      name: "Everyday",
+      artist: "Aves",
+      cover: "https://i.scdn.co/image/ab67616d0000b273c7ca2ada3536de8f9d570732",
+      id: uuidv4(),
+      active: true,
+      color: ["#3f564e", "#91a390"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41890",
+    },
+    {
+      //2
+      name: "A Reminder",
+      artist: "Sleepy Fish, coa",
+      cover: "https://i.scdn.co/image/ab67616d0000b27326b04e87d8297b1b507c2cd8",
+      id: uuidv4(),
+      active: false,
+      color: ["#be4e2b", "#fc7203"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=36925",
+    },
+    {
+      //3
+      name: "Making a Way",
+      artist: "The BREED",
+      cover: "https://i.scdn.co/image/ab67616d0000b27368b3919dab921f1452202bb0",
+      id: uuidv4(),
+      active: false,
+      color: ["#99412b", "#bc9631"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41956",
+    },
+    {
+      //4
+      name: "Passing Storm",
+      artist: "Moderator",
+      cover: "https://i.scdn.co/image/ab67616d0000b2732ba2b7bbc1537c41ca67543d",
+      id: uuidv4(),
+      active: false,
+      color: ["#1f322c", "#bc9355"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=42056",
+    },
+    {
+      //5
+      name: "In Minutes",
+      artist: "Shrimpnose",
+      cover: "https://i.scdn.co/image/ab67616d0000b27382649b5b0d2af7f4e506d3a6",
+      id: uuidv4(),
+      active: false,
+      color: ["#e4b1b6", "#6a85b4"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41689",
+    },
+    {
+      //6
+      name: "Night Owl",
+      artist: "brillion.",
+      cover: "https://i.scdn.co/image/ab67616d0000b27344c53b2c3a1457e1498ce55e",
+      id: uuidv4(),
+      active: false,
+      color: ["#9773f7", "#47247c"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=35641",
+    },
+    {
+      //7
+      name: "Blessed",
+      artist: "Moods, Yasper",
+      cover: "https://i.scdn.co/image/ab67616d0000b273e5852cd50201ae82191cc7ba",
+      id: uuidv4(),
+      active: false,
+      color: ["#051223", "#a9847c"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=32860",
+    },
+  ];
+}
+
+export default chillHop();
